dfc98d1676d0c9f82b2c12ba05f05eec518948af
conqueror-G <gitconquerorg@gmail.com>
2022-06-20 19:19:06 +0900

    ADD: 초기세팅 완료

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Nav.scss";
+
+const Nav = () => {
+  return <div>fdsfs</div>;
+};
+
+export default Nav;
---
+++ src/pages/Details/Detail.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Detail.scss";
+
+const Detail = () => {
+  return <div>fdsafs</div>;
+};
+
+export default Detail;
