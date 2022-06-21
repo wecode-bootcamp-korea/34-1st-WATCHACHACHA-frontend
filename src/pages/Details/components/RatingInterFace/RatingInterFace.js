ada143a85cd7e15851c97d3aff80a950ff499eff
conqueror-G <gitconquerorg@gmail.com>
2022-06-21 18:07:35 +0900

    ADD: 사용자와 상호작용하는컴포넌트 레이아웃 완료

--- src/pages/Details/components/RatingInterFace/RatingInterFace.js
+++ src/pages/Details/components/RatingInterFace/RatingInterFace.js
@@ -6,10 +6,51 @@ const RatingInterFace = () => {
     <div className="ratingInterface">
       <div className="poster">이미지 태그 써야할 곳</div>
       <div className="ratingDetail">
-        <h1>ratingTitle</h1>
-        <div>ratingInfo</div>
-        <div>ratinghandling</div>
-        <div>ratingUserInterface</div>
+        <h1 className="ratingTitle">
+          <p className="movieTitle">마녀&#40;魔女 Part2. The Other One</p>
+          <p className="movieCategory">2021 ・ 한국 ・ 액션</p>
+        </h1>
+        <div className="ratingInfo">평균 ★2.8 &#40;6,414명&#41;</div>
+        <div className="ratinghandling">
+          <p className="rating">평가하기</p>
+          <img src="/images/icon/star.png" alt="ratingStar" />
+        </div>
+        <div className="ratingUserInterface">
+          <div className="interfaceWrapper">
+            <button className="iconButton">
+              <img
+                className="iconImage"
+                src="/images/icon/positiveAddIcon.png"
+                alt="bookmaker"
+              />
+              <span className="iconText">보고싶어요</span>
+            </button>
+            <button className="iconButton">
+              <img
+                className="iconImage"
+                src="/images/icon/penIcon.png"
+                alt="bookmaker"
+              />
+              <span className="iconText">코멘트</span>
+            </button>
+            <button className="iconButton">
+              <img
+                className="iconImage"
+                src="/images/icon/eyeIcon.png"
+                alt="bookmaker"
+              />
+              <span className="iconText">보는중</span>
+            </button>
+            <button className="iconButton">
+              <img
+                className="iconImage"
+                src="/images/icon/forbiddenIcon.png"
+                alt="bookmaker"
+              />
+              <span className="iconText">관심없어요</span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
