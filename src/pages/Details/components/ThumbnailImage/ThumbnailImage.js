f1ccb1c259045ee684b9321e3259d8f6154e24bd
conqueror-G <gitconquerorg@gmail.com>
2022-06-21 11:04:01 +0900

    ADD: ThumbnailImage.js 컴포넌트 생성

---
+++ src/pages/Details/components/ThumbnailImage/ThumbnailImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RankingInfoList from "../RankingInfoList/RankingInfoList";
+import "./ThumbnailImage.scss";
+
+const ThumbnailImage = () => {
+  return (
+    <div className="thumbnailImage">
+      <RankingInfoList />
+    </div>
+  );
+};
+
+export default ThumbnailImage;
