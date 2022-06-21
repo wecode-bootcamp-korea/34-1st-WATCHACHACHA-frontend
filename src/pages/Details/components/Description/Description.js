9eb846fba29e8539fd7a01d7b00490fb62dd5fb2
conqueror-G <gitconquerorg@gmail.com>
2022-06-21 11:20:11 +0900

    ADD: Description.js 컴포넌트 생성

---
+++ src/pages/Details/components/Description/Description.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Description.scss";
+
+const Description = () => {
+  return (
+    <div className="description">
+      <div>상세설명임</div>
+    </div>
+  );
+};
+
+export default Description;
