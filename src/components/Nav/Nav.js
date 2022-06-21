e92333e453e57658052c33badc13bdeb30c09fe2
conqueror-G <gitconquerorg@gmail.com>
2022-06-21 10:57:27 +0900

    ADD: detail.js 그리드 레이아웃 구조 생성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Nav.scss";
 
 const Nav = () => {
-  return <div>fdsfs</div>;
+  return <div />;
 };
 
 export default Nav;
--- src/pages/Details/Detail.js
+++ src/pages/Details/Detail.js
@@ -1,8 +1,17 @@
 import React from "react";
+import ThumbnailImage from "./components/ThumbnailImage/ThumbnailImage";
+import Infomation from "./components/Infomation/Infomation";
+import DescriptionCard from "./components/DescriptionCard/DescriptionCard";
 import "./Detail.scss";
 
 const Detail = () => {
-  return <div>fdsafs</div>;
+  return (
+    <div className="detailPage">
+      <ThumbnailImage />
+      <Infomation />
+      <DescriptionCard />
+    </div>
+  );
 };
 
 export default Detail;
