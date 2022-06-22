7a7fe167b4f06fee7342c450e88c20b0d16cae57
JoahKim <kimek4319@gmail.com>
2022-06-22 13:40:33 +0900

    mainpage mock data 생성

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
 import "./Main.scss";
 import FirstLow from "./components/FirstLow/FirstLow";
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch("/data/mainMock.json")
+      .then(res => res.json())
+      .then(data => {
+        setMovies(data);
+      });
+  }, []);
   return (
     <>
       <div>
@@ -13,22 +21,21 @@ const Main = () => {
         <div className="mainBox1">
           <p className="filmTheme1">1970's Films</p>
           <ul className="filmList">
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
-            <FirstLow />
+            {movies.map(movie => (
+              <FirstLow
+                key={movie.id}
+                id={movie.id}
+                name={movie.name}
+                date={movie.release_date}
+                url={movie.image_url}
+              />
+            ))}
           </ul>
           <div className="preBtn">
-            <i class="fa-solid fa-chevron-left" />
+            <i className="fa-solid fa-chevron-left" />
           </div>
           <div className="nextBtn">
-            <i class="fa-solid fa-chevron-right" />
+            <i className="fa-solid fa-chevron-right" />
           </div>
         </div>
         <div className="mainBox2">
--- src/pages/Main/components/FirstLow/FirstLow.js
+++ src/pages/Main/components/FirstLow/FirstLow.js
@@ -2,20 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./FirstLow.scss";
 
-const FirstLow = () => {
+const FirstLow = ({ id, name, date, url }) => {
   return (
-    <li className="firstLow">
+    <li className="firstLow" id={id}>
       <Link to="/details" className="film1Detail">
         <div className="posterBox">
-          <img
-            src="/images/poster/TheGreatGatsby.jpg"
-            alt="개츠비"
-            className="filmPosters"
-          />
+          <img src={url} alt="개츠비" className="filmPosters" />
         </div>
         <div className="filmDescription">
-          <p className="filmName">위대한 개츠비</p>
-          <p className="filmYear">1920 ▪ 미국</p>
+          <p className="filmName">{name}</p>
+          <p className="filmYear">{date} ▪ 미국</p>
           <p className="filmAverage">평균★3.7</p>
         </div>
       </Link>
