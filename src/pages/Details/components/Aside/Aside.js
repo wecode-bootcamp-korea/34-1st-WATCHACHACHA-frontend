5fc8227bdef54d6439103600351ae4c5a9f3a77a
conqueror-G <gitconquerorg@gmail.com>
2022-06-22 23:18:27 +0900

    ADD: 상세페이지 레이아웃 구현 완료

--- src/pages/Details/components/Aside/Aside.js
+++ src/pages/Details/components/Aside/Aside.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./Aside.scss";
 
 const Aside = () => {
-  return <aside className="media">Media components</aside>;
+  return (
+    <aside className="aside">
+      <section class="gallery">
+        <h2 className="galleryTitle">갤러리</h2>
+        <ul className="galleryList">
+          <li className="galleryListItem">
+            <img className="filmsImage" />
+          </li>
+          <li className="galleryListItem">
+            <img className="filmsImage" />
+          </li>
+          <li className="galleryListItem">
+            <img className="filmsImage" />
+          </li>
+        </ul>
+      </section>
+    </aside>
+  );
 };
 
 export default Aside;
