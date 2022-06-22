e1d8401a7573b45bbb1be950a8e9c83c8bd50b9e
conqueror-G <gitconquerorg@gmail.com>
2022-06-22 15:51:49 +0900

    ADD: FilmCrew 컴포넌트 생성

--- src/pages/Details/components/Description/Description.js
+++ src/pages/Details/components/Description/Description.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BasicInfo from "../BasicInfo/BasicInfo";
+import FilmCrew from "../FilmCrew/FilmCrew";
 import "./Description.scss";
 
 const Description = () => {
   return (
     <section className="description">
       <BasicInfo />
+      <FilmCrew />
     </section>
   );
 };
