66fafc37ab5b7d66e676b549c5b1839569d94091
conqueror-G <gitconquerorg@gmail.com>
2022-06-23 10:32:47 +0900

    EDIT: 영화 정보 섹션 수정

--- src/pages/Details/components/Information/Information.js
+++ src/pages/Details/components/Information/Information.js
@@ -7,13 +7,17 @@ const Information = () => {
       <div className="ratingInterface">
         <div className="poster" />
         <div className="ratingDetail">
-          <h1 className="ratingTitle">
-            <p className="movieTitle">
+          <div className="ratingTitle">
+            <h1 className="movieTitle">
               마녀&#40;魔女&#41; Part2. The Other One
-            </p>
+            </h1>
             <p className="movieCategory">2021 ・ 한국 ・ 액션</p>
-          </h1>
-          <div className="ratingInfo">평균 ★2.8 &#40;6,414명&#41;</div>
+          </div>
+          <div className="ratingInfo">
+            <span className="ratingInfoText">평균</span>
+            <span className="ratingAverage">{`★${"2.8"}`}</span>
+            <span className="ratingSumPerson">&#40;6,414명&#41;</span>
+          </div>
           <div className="ratingStar">
             <p className="rating">평가하기</p>
             <img src="/images/icon/star.png" alt="ratingStar" />
