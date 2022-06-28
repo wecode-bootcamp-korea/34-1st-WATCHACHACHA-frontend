1d47c458355e9bb4ccb2f741b934e9a4208b911e
JoahKim <kimek4319@gmail.com>
2022-06-28 18:44:29 +0900

    footer 구현 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,28 +14,35 @@ const Footer = () => {
           <article className="service">
             <ul className="customerService">
               <li>서비스 이용약관</li>
+              <li>ㅣ</li>
               <li>개인정보 처리방침</li>
+              <li>ㅣ</li>
               <li>회사 안내</li>
             </ul>
           </article>
           <article className="contact">
             <ul className="toTheWeb">
               <li>고객센터</li>
+              <li>ㅣ</li>
               <li>https://wecode.co.kr/</li>
             </ul>
             <ul>
               <li>광고문의</li>
+              <li>ㅣ</li>
               <li>https://wecode.co.kr/</li>
             </ul>
             <ul>
               <li>제휴 및 대외 협력</li>
+              <li>ㅣ</li>
               <li>https://wecode.co.kr/</li>
             </ul>
           </article>
           <article className="detailInformation">
             <ul>
               <li>주식회사 왓챠</li>
+              <li>ㅣ</li>
               <li>대표 박태훈</li>
+              <li>ㅣ</li>
               <li>서울특별시 서초구 강남대로 343 신덕빌딩 3층</li>
             </ul>
             <ul>
@@ -47,21 +54,19 @@ const Footer = () => {
             </ul>
           </article>
         </section>
-        <section>
-          <article className="socialLanguage">
-            <div className="selections">
-              <select className="languages">
-                <option value="한국어" key="한국어">
-                  한국어
-                </option>
-              </select>
-            </div>
-            <div className="icons">
-              <i className="fa-brands fa-facebook" />
-              <i className="fa-brands fa-twitter" />
-              <i className="fa-solid fa-b" />
-            </div>
-          </article>
+        <section className="socialLanguage">
+          <div className="selections">
+            <select className="languages">
+              <option value="한국어" key="한국어" className="option">
+                한국어
+              </option>
+            </select>
+          </div>
+          <div className="icons">
+            <i className="fa-brands fa-facebook" />
+            <i className="fa-brands fa-twitter" />
+            <i className="fa-solid fa-b" />
+          </div>
         </section>
       </div>
     </footer>
